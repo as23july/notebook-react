15f74864179a6e4d602ddda659adf3cdd38842bf
Aditya Singh <as23july@gmail.com>
2024-02-11 16:05:11 +0530

    new commit to hoste

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,15 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '../build')))
+
 // avalable Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
-
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
